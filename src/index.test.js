d6d51a5bf56670057e02d8e1a1dd03b0603f881b
George Aidonidis <george.aidonidis@gmail.com>
2018-02-25 21:39:22 +0200

    Add test
    * Change double quotes to single
    * Test empty string input

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,15 @@
-const safelyParseUri = require(".");
+const safelyParseUri = require('.');
 
-describe("safelyParseUri", () => {
-  test("encodes path when special characters are in place", () => {
-    const cat = "cat";
-    const weirdCat = "cat?format=json";
-    const uriPath = safelyParseUri`/${cat}/${weirdCat}`;
-    expect(uriPath).toEqual("/cat/cat%3Fformat%3Djson");
-  });
+describe('safelyParseUri', () => {
+	test('encodes path when special characters are in place', () => {
+		const cat = 'cat';
+		const weirdCat = 'cat?format=json';
+		const uriPath = safelyParseUri`/${cat}/${weirdCat}`;
+		expect(uriPath).toEqual('/cat/cat%3Fformat%3Djson');
+	});
+
+	test('Returns empty string when empty string is passed', () => {
+		const uriPath = safelyParseUri``;
+		expect(uriPath).toEqual('');
+	});
 });
